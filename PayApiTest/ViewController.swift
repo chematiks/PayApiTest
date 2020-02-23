268a6851fb9eb3eedce7ba5e2f8091ba1371fc6b
Alex Chanchikov <chematiks@ya.ru>
2020-02-23 23:25:28 +0700

    init commit

---
+++ PayApiTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PayApiTest
+//
+//  Created by ALEXANDER CHANCHIKOV on 23.02.2020.
+//  Copyright © 2020 ChematiksLabs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
